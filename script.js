6f87a0521c7f14e4e48d8f7dff72c7d593d248f2
Tileryr <tileryrchicken@gmail.com>
2024-12-03 16:40:34 -0500

    added sounds

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-function tyler(jokes, and ) {
-    document
-    do
-    
+var primaryMouseButtonDown = false;
+
+function setPrimaryButtonState(e) {
+  var flags = e.buttons !== undefined ? e.buttons : e.which;
+  primaryMouseButtonDown = (flags & 1) === 1;
 }
+
+function mouseOverString(e) {
+    if(primaryMouseButtonDown) {
+        play()
+    }
+}
+
+function play(note) {
+  const audio = new Audio(`./sounds/E.ogg`);
+  audio.play()
+}
+document.addEventListener("mousedown", setPrimaryButtonState);
+document.addEventListener("mousemove", setPrimaryButtonState);
+document.addEventListener("mouseup", setPrimaryButtonState);
+
+const strings = document.querySelectorAll(".string")
+strings.forEach(string => string.addEventListener("mouseover", mouseOverString))
